141b3794affa8f9632d4ea701e1e81467dd228d5
Filipp Pinskiy <pinskiy.fs@ya.ru>
2022-12-07 20:48:44 +0300

    Внес исправления

--- Lesson_9/cart.js
+++ Lesson_9/cart.js
@@ -5,23 +5,25 @@ const cart = {
     get totalPrice() {
         return this.calculateItemPrice();
     },
-    getPrice:[], // добавил массив что бы хранить цену и использовать reduce
     count: 0,
     
-    add(item, price, n = 1) {
-        this.items.push(item);
-        this.getPrice.push(price * n);
-        this.count = this.count + n;
+    add(name, price, count = 1) {
+        const newProduct = {
+            name,
+            price,
+            count,
+        };
+        this.items.push(newProduct);
+        this.increaseCount(count);
     }, 
     increaseCount(n) {
         return this.count = this.count + n;
     },
     calculateItemPrice() {
-        return this.getPrice.reduce((acc, item) => {
-            return acc + item;
+        return this.items.reduce((acc, item) => {
+            return acc + item.price * item.count;
         }, 0);
     },
-        
     clear() {
         this.items = [];
         this.totalPrice = 0 ;
@@ -37,6 +39,6 @@ cart.add('samsung', 30000, 5);
 cart.add('nokia', 17000, 2);
 cart.add('ihone', 50000, 3);
 
-cart.print();
 
+cart.print();
 
--- Test/cart.js
+++ Test/cart.js
@@ -1,22 +1,65 @@
-let user = {
-    name: "John",
-    surname: "Smith"
-  };
-  
-  Object.defineProperty(user, 'fullName', {
-    get() {
-      return `${this.name} ${this.surname}`;
-    },
-  
-    set(value) {
-      [this.name, this.surname] = value.split(" ");
-    }
-  });
-  
-  user.fullName = 'aaa';
+const cart = {
+	items: [],
+	count: 0,
+	
 
-  console.log(user.fullName); // John Smith
-  
-  
-  for(let key in user) console.log(key); // name, surname
-  
+	get totalPrice () {
+		return this.calculateItemPrice();
+
+	},
+
+	calculateItemPrice() {
+    const priceWithoutDiscount = this.items.reduce((sum, item) => 
+        sum + item.productPrice * item.productCount, 0);
+        return priceWithoutDiscount;
+    },
+
+
+//   const priceWithoutDiscount = this.items.reduce((sum, item) =>
+// sum + item.price * item.quantity, 0);
+
+// return priceWithoutDiscount - this.discount / 100 * priceWithoutDiscount;
+// },
+
+		// let totalPrice = 0;
+		// this.items.forEach(item => {
+		// 	const itemPrice = item.productPrice * item.productCount;
+		// 	totalPrice += itemPrice;
+			// console.log(totalPrice);
+		// return totalPrice;
+
+	add(productName, productPrice, productCount = 1) {
+
+		const newItem = { productName, productPrice, productCount };
+		this.items.push(newItem);
+		this.increaseCount(productCount);
+		
+	},
+
+	increaseCount(counter) {
+		return this.count += counter;
+	},
+	
+  print() {
+    console.log(cart.items);
+    console.log(this.totalPrice);
+},
+};
+
+// Performing tests for metheds of cart object:
+
+cart.add('samsung', 30000, 5);
+cart.add('nokia', 17000, 2);
+cart.add('ihone', 50000, 3);
+// console.log("🚀 ~ file: cart.js ~ line 60 ~ add ~ items", cart.items);
+cart.totalPrice;
+//cart.print();
+
+
+items: [
+  {
+    nameProduct: 'samsung',
+    priceProduct: 30000,
+    countProduct: 5
+  }
+]
