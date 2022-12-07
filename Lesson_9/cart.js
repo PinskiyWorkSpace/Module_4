ffd3daf90adbd5b66c782ca5f22fc322f3a966bd
Filipp Pinskiy <pinskiy.fs@ya.ru>
2022-12-07 21:15:05 +0300

    Исправлено

--- Lesson_9/cart.js
+++ Lesson_9/cart.js
@@ -5,30 +5,32 @@ const cart = {
     get totalPrice() {
         return this.calculateItemPrice();
     },
-    getPrice:[], // добавил массив что бы хранить цену и использовать reduce
     count: 0,
     
-    add(item, price, n = 1) {
-        this.items.push(item);
-        this.getPrice.push(price * n);
-        this.count = this.count + n;
+    add(name, price, count = 1) {
+        const newProduct = {
+            name,
+            price,
+            count,
+        };
+        this.items.push(newProduct);
+        this.increaseCount(count);
     }, 
     increaseCount(n) {
         return this.count = this.count + n;
     },
     calculateItemPrice() {
-        return this.getPrice.reduce((acc, item) => {
-            return acc + item;
+        return this.items.reduce((acc, item) => {
+            return acc + item.price * item.count;
         }, 0);
     },
-        
     clear() {
         this.items = [];
         this.totalPrice = 0 ;
         this.count = 0 ;
     },
     print() {
-        console.log(JSON.stringify(this.items));
+        console.log(JSON.stringify(this.items).padStart(''));
         console.log(this.totalPrice);
     },
 };
@@ -37,6 +39,6 @@ cart.add('samsung', 30000, 5);
 cart.add('nokia', 17000, 2);
 cart.add('ihone', 50000, 3);
 
-cart.print();
 
+cart.print();
 
