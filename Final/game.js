b541e52ced7f9bb7ebc715bfc7ef2f65fb941139
Filipp Pinskiy <pinskiy.fs@ya.ru>
2022-12-22 19:11:29 +0300

    Исправил замечание

--- Final/game.js
+++ Final/game.js
@@ -19,39 +19,48 @@
       console.log('player: ', player);
 
       const getPlayer = () => {
-        if (player === null) {
-          player = confirm('exit?');
-          if (player) {
-            alert(`Всего доброго!!!`);
-          } else {
-            start();
+        if (!isNaN(player)) {
+          if (player === null) {
+            player = confirm('exit?');
+            if (player) {
+              alert(`Всего доброго!!!`);
+            } else {
+              start();
+            }
+          } else if (player === '' || player < 1) {
+            player = prompt('Попробуйде ввести правильное число шаров!');
+          } else if (player > result.player) {
+            player = prompt('Не хватает шаров!');
+            getPlayer();
           }
-        } else if (player === '' || player < 1) {
-          player = prompt('Попробуйде ввести правильное число шаров!');
-        } else if (player > result.player) {
-          player = prompt('Не хватает шаров!');
-          getPlayer();
-        }
+      } else {
+        player = prompt('Вы ввели не число!');
+        getPlayer();
       };
+    };
       getPlayer();
 
 
       const comp = getRandomIntInclusive(1, result.computer);
       console.log('comp: ', comp);
 
-      if (player % 2 === comp % 2) {
-        console.log('Угадал! ');
-        result.player -= +player;
-        result.computer += +player;
-        console.log(result.player);
-        console.log(result.computer);
+      if (comp % 2 === 0) {
+        if (player % 2 === 0) {
+          alert('Чет!')
+          result.player -= +player;
+          result.computer += +player;
+          alert(`Компьютер угадал!
+          Шариков у игрока ${result.player}
+          Шариков у компьютера ${result.computer}`)
+        };
       } else {
-        console.log('Не угадал! ');
+        alert('Нечет!')
         result.player += +player;
         result.computer -= +player;
-        console.log(result.player);
-        console.log(result.computer);
-      }
+        alert(`Игрок угадал!
+        Шариков у игрока ${result.player}
+        Шариков у компьютера ${result.computer}`)
+      };
 
       if (result.computer <= 0) {
         alert('Подедил игрок');
